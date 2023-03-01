a08f23436426f0cf70ea717635968a5c4d0f1116
Atsiavor Kojo Israel <atsiavorisrael@gmail.com>
2023-02-28 17:27:50 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,18 +5,25 @@
  * return: (o)
  */
 void print_rev(char *s)
+
 {
 	int longi = 0;
 	int (o);
 
+
+
+
+
 	while (*s != '\0')
+
 	{
 		longi++;
 		s++;
 	}
 	s--;
-	for (o  = longi; o > 0; o-- )
+	for (o  = longi; o > 0; o--)
 	{
+
 		_putchar(*s);
 		s--;
 	}
