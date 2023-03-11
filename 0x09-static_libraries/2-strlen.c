d6e4a9d8497ee43e92d223f8233bc975e52e661e
Atsiavor Kojo Israel <atsiavorisrael@gmail.com>
2023-03-10 16:52:24 -0800

    libmy.a

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -7,12 +7,15 @@
  *
  * Return: The length of @str.
  */
-size_t  _strlen(const char *str)
+int _strlen(char *str)
 {
 	size_t length = 0;
 
-	while (*str++)
+	while (*str)
+	{
 		length++;
+		str++;
+	}
 
 	return (length);
 }
