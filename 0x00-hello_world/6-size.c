76309b5341230b1209b64b7f9d1c5a901e9b6155
Israel <atsiavorisrael>
2023-05-18 22:48:23 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,11 @@ int main(void)
 int intType;
 float floatType;
 long int longType;
-long long int longlongtype;
 char charType;
 
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("Size of long int: %zu bytes\n", sizeof(longType));
-printf("Size of long long int: %zu bytes\n", sizeof(longlongType));
 printf("Size of char: %zu byte\n", sizeof(charType));
 return (0);
 }
