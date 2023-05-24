37ba8349d65b90b5572d2b1f8fc47aa5ea2bdf07
Israel <atsiavorisrael>
2023-05-23 18:24:53 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,16 @@ long int longint;
 char charType;
 long long int longlongint;
 
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of long long int: %zu bytes\n", sizeof(longlongint));
-printf("Size of long int: %zu bytes\n", sizeof(longint));
-printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of long  int: %zu byte(s)\n", sizeof(longint));
+printf("Size of long long int: %zu byte(s)\n", sizeof(longlongint));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 
 return (0);
 }
+Size of a char: 1 byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 4 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
