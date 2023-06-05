01186740431accc2f48907f72cd6afdc07af84f0
Israel <atsiavorisrael>
2023-06-05 00:26:31 -0700

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * more_numbers- prints
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+int i, j;
+
+i = 0;
+while (i < 10)
+
+{
+j = 0;
+{while ( j <= 14);
+_putchar(j);
+j++;
+}
+_putchar(i);
+}
+_putchar('\n');
+}
+
