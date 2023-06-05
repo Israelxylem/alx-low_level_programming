53ca699f7c68e20ef63e9245ec70fceddbba1f63
Israel <atsiavorisrael>
2023-06-04 23:12:57 -0700

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_numbers - 0123456789
+ *
+ * Return:  void
+ */
+void print_numbers(void)
+{
+char l;
+for (l = 0; l <= 9; l++)
+{
+_putchar(l);
+}
+_putchar('\n');
+}
