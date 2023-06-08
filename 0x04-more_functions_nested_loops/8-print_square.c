e8b85c6628a1bc7b3a770ff1a94009c6bbae85e4
Israel <atsiavorisrael>
2023-06-08 08:29:48 -0700

    update

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * print_square- prints a square
+ * @size: the numbers of times '#' is printed
+ * description: prints a square
+ */
+
+void print_square(int size)
+{
+int i, j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+printf("*");
+}
+printf("\n");
+}
+printf("\n");
+}
