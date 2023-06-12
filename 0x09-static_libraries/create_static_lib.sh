e90e796d2a2e8f07f862538364238183ba0952eb
Israel <atsiavorisrael>
2023-06-12 11:19:49 -0700

    update

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-gcc -c *.c
- ar -rc liball.a *.o
+
+# Compile each .c file into object files
+for file in *.c; do
+    gcc -c "$file" -o "${file%.c}.o"
+done
+
+# Create the static library (liball.a)
+ar rcs liball.a *.o
+
+# Clean up object files
+rm *.o
